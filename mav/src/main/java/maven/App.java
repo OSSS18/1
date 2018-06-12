c419ec724b382924ceed0fd1412f03e9d7b35eb2
OSSS18 <40203986+OSSS18@users.noreply.github.com>
2018-06-12 15:13:25 +0200

    Add files via upload

---
+++ mav/src/main/java/maven/App.java
@@ -0,0 +1,19 @@
+package maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+	for( int i = 0; i < args.length; i++ )
+	{
+		System.out.println( args[i].toUpperCase() );
+	}
+	
+	
+    }
+}
